ed38c46d5201562b906d63d00900ef774b559526
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-06-26 13:51:38 +0500

    Creating models for dash api

--- dash_api/models.py
+++ dash_api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Equipment_Type(models.Model):
+    type = models.CharField(max_length=55)
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255)
+    equipment_type = models.ForeignKey(Equipment_Type, on_delete=models.PROTECT)
+
+
+class Aggreagation_Type(models.Model):
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=10)
+
+
+class Aggregation(models.Model):
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    aggregation_type = models.ForeignKey(
+        Aggreagation_Type, on_delete=models.PROTECT)
+    value = models.DecimalField(max_digits=10, decimal_places=6)
+    timestamp = models.DateTimeField()
